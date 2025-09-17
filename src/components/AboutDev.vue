16cf77518d8aea51148b5c0a70b1ebbd55d0db91
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-18 00:55:44 +0700

    love

--- src/components/AboutDev.vue
+++ src/components/AboutDev.vue
@@ -3,6 +3,6 @@
     href="https://github.com/ukasyaaah"
     target="_blank"
     class="animate__animated animate__tada flex  button  items-center shadow-md shadow-tahiti/30 justify-center absolute left-3 top-3 bg-tahiti w-13 h-13"
-  >{{ '</>' }}
+  >&hearts;
   </a>
 </template>
