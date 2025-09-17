01665ac0676b733e35625fe5059f50f5bf4b16d9
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-17 23:25:00 +0700

    semi final

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import { computed, reactive } from "vue";
+import { computed, isReactive, reactive } from "vue";
 import BaseLayout from "./layouts/BaseLayout.vue";
+import ResultPage from "./pages/ResultPage.vue";
 
 const kocok = reactive({
   inputName: "",
-  names: [],
+  names: ["Ari", "Budi", "Citra", "Dewi", "Eka"],
   error: "",
   state: true,
 });
 
 const isReady = computed(() => (kocok.names.length > 1 ? true : false));
-
-function notState() {
-  kocok.state = !kocok.state;
-}
 
 function addNameToList() {
   const name = kocok.inputName;
@@ -42,13 +39,29 @@ function validate(value) {
     return true;
   }
 }
+
+function getRandomName() {
+  return kocok.names[Math.floor(Math.random() * kocok.names.length)];
+}
+
+function generateResult() {
+  kocok.state = false;
+}
+
+function resetApp() {
+  kocok.inputName = "";
+  kocok.names = [];
+  kocok.error = "";
+  kocok.state = true;
+}
 </script>
 
 <template>
   <BaseLayout :vif="kocok.state">
     <div class="md:flex mx-5 md:gap-20 items-center lg:gap-40">
       <div
-        class="flex flex-col justify-around h-90 shadow-2xl shadow-tahiti items-center max-w-sm md:max-w-md mx-auto p-15 rounded-2xl bg-tahiti text-teks"
+        :class="isReady ? 'h-90' : 'h-70'"
+        class="flex flex-col justify-around shadow-2xl shadow-tahiti items-center max-w-sm md:max-w-md mx-auto p-15 rounded-2xl bg-tahiti text-teks"
       >
         <h1 class="font-bold text-3xl">Kocok Orang</h1>
         <form class="flex gap-x-4 justify-center">
@@ -73,7 +86,7 @@ function validate(value) {
 
         <button
           class="button animate__animated animate__fadeInDown"
-          @click.prevent="notState"
+          @click.prevent="generateResult"
           v-if="isReady"
         >
           Kocok Sekarang!
@@ -98,5 +111,9 @@ function validate(value) {
     </div>
   </BaseLayout>
 
-  <BaseLayout :vif="!kocok.state"> miaw</BaseLayout>
+  <ResultPage
+    :isActive="!kocok.state"
+    :reset-app="resetApp"
+    :get-random-name="getRandomName"
+  />
 </template>
---
+++ src/pages/ResultPage.vue
@@ -0,0 +1,19 @@
+<script setup >
+import BaseLayout from "../layouts/BaseLayout.vue";
+
+const { isActive, resetApp, getRandomName } = defineProps([
+  "isActive",
+  "resetApp",
+  "getRandomName",
+]);
+</script>
+
+<template>
+  <BaseLayout :vif="isActive">
+    <div class="flex flex-col justify-around h-70 items-center">
+      <h1 class="text-xl font-semibold">Pemenangnya adalah</h1>
+      <p class="text-9xl text-tahiti font-bold">{{ getRandomName() }}!</p>
+      <button @click="resetApp" class="button">Mulai Lagi</button>
+    </div>
+  </BaseLayout>
+</template>
