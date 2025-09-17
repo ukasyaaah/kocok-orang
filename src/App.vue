b4dd3738c759b2ec8749324c8dfeb1a4acd6d044
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-17 17:30:54 +0700

    validation name

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
+
+const kocok = reactive({
+  inputName: "",
+  names: ["ari", "arfi"],
+  error: "",
+});
+
+function addNameToList() {
+  const name = kocok.inputName;
+  if (validate(name)) {
+    kocok.names.push(name);
+    kocok.inputName = "";
+  } else {
+    return kocok.error;
+  }
+}
+
+function validate(value) {
+  if (!value) {
+    kocok.error = "Nama Ga Boleh Kosong!";
+    return false;
+  } else if (kocok.names.includes(value)) {
+    kocok.error = "Nama Ga Boleh Sama!";
+    return false;
+  } else {
+    return true;
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Kocok Orang</h1>
+  <form class="input-form">
+    <input type="text" v-model="kocok.inputName" />
+    <button @click.enter.prevent="addNameToList">Tambah</button>
+  </form>
+  <h3 v-if="kocok.error">
+    {{ kocok.error }}
+  </h3>
+  <button v-for="nameh in kocok.names">
+    {{ nameh }}
+  </button>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.input-form {
+  margin: 0;
+
+  button {
+    border: 2px solid black;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+
+  input {
+    padding: 11.3px;
+    border: 2px solid black;
+    border-top-left-radius: 12px;
+    border-bottom-left-radius: 12px;
+  }
 }
 </style>
