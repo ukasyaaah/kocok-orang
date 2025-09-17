ca0d2d7d315b2c3172defb4f5c7c3c835796aa74
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-18 00:47:50 +0700

    final

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,11 +1,17 @@
 <script setup>
+import AboutDev from '../components/AboutDev.vue';
+
 defineProps({
   vif: Boolean,
 });
 </script>
 
 <template>
-  <div v-if="vif" class="animate__animated animate__bounceInUp h-screen justify-center items-center mx-auto flex">
+  <AboutDev/>
+  <div
+    v-if="vif"
+    class="animate__animated animate__bounceInUp h-screen justify-center items-center mx-auto flex"
+  >
     <slot />
   </div>
 </template>
